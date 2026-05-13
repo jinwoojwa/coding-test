bc839f958e0b1761d13d060ab18abb12a3722e41
Jinwoo <112393728+jinwoojwa@users.noreply.github.com>
2026-05-13 12:45:56 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 3.82 ms, Memory: 62.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    
+    static Map<String, Integer> termsMap = new HashMap<>();
+    
+    public static int changeDateFormat(String date) {
+        String[] splitedDate = date.split("\\.");
+        
+        int totalDays = 0;
+        
+        // 2000 <= year <= 2022
+        totalDays += Integer.parseInt(splitedDate[0].substring(2)) * 12 * 28;
+        totalDays += Integer.parseInt(splitedDate[1]) * 28;
+        totalDays += Integer.parseInt(splitedDate[2]);
+        
+        return totalDays;
+    }
+    
+    public static int calcEndDate(String privacy) {
+        String[] splitedPrivacy = privacy.split(" ");
+        
+        int privacyDate = changeDateFormat(splitedPrivacy[0]);
+        String term = splitedPrivacy[1];
+        
+        return privacyDate + termsMap.get(term);
+    }
+    
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for (String term : terms) {
+            String[] splitedTerm = term.split(" ");
+            
+            int termsToDays = Integer.parseInt(splitedTerm[1]) * 28;
+            termsMap.put(splitedTerm[0], termsToDays);
+        }
+        
+        for (int i = 0; i < privacies.length; ++i) {
+            int endDate = calcEndDate(privacies[i]);
+            
+            if (changeDateFormat(today) >= endDate) answer.add(i + 1);
+        }
+        
+        return answer.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
+    }
+}

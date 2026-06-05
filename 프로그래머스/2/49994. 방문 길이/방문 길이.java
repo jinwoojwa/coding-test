87852c7e620468efc62f30c2df97f7efa9e37afc
Jinwoo <112393728+jinwoojwa@users.noreply.github.com>
2026-06-05 10:29:36 +0900

    [level 2] Title: 방문 길이, Time: 0.16 ms, Memory: 64.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,52 @@
+class Solution {
+    
+    int[] dx = {0, 1, 0, -1}; // R, D, L, U
+    int[] dy = {1, 0, -1, 0};
+    
+    public int solution(String dirs) {
+        int answer = 0;
+        
+        boolean[][][] visited = new boolean[11][11][4]; // 시작 좌표: (5, 5)
+        int x = 5, y = 5;
+        
+        for (int i = 0; i < dirs.length(); ++i) {
+            int dir = getDir(dirs.charAt(i));
+            int reverseDir = getDir(reverseDirection(dirs.charAt(i)));
+            
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            
+            if (nx < 0 || ny < 0 || nx > 10 || ny > 10) continue;
+            
+            // 아직 방문 X
+            if (!visited[nx][ny][dir]) answer++;
+            
+            visited[nx][ny][dir] = true;
+            visited[x][y][reverseDir] = true;
+            x = nx; y = ny;
+        }
+        return answer;
+    }
+    
+    private int getDir(char c) {
+        return switch (c) {
+                case 'R' -> 0;
+                case 'D' -> 1;
+                case 'L' -> 2;
+                case 'U' -> 3;
+                default -> -1;
+        };
+    }
+    
+    private char reverseDirection(char c) {
+        return switch (c) {
+                case 'R' -> 'L';
+                case 'D' -> 'U';
+                case 'L' -> 'R';
+                case 'U' -> 'D';
+                default -> 'X';
+        };
+    }
+}
+
+// 같은 지점에 방문했더라도 어느 방향에서 방문했는지에 따라 길이 다름 (방문 표시에 방향 정보도 넣어야 함)

5ef24575c717d75d12a1d54f5e7f68fd4802e5f2
Jinwoo <112393728+jinwoojwa@users.noreply.github.com>
2026-06-09 09:33:13 +0900

    [level 3] Title: 카운트 다운, Time: 29.35 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -1,57 +1,52 @@
 import java.util.*;
 
 class Solution {
-
+    record Score(int point, int singleOrBull) {}
+    
     public int[] solution(int target) {
-
         int[][] dp = new int[target + 1][2];
-
-        for (int i = 1; i <= target; i++) {
-            dp[i][0] = Integer.MAX_VALUE;
+        
+        for (int i = 1; i <= target; ++i) {
+            dp[i][0] = 100001;
         }
-
-        List<int[]> scores = new ArrayList<>();
-
-        // 싱글
-        for (int i = 1; i <= 20; i++) {
-            scores.add(new int[]{i, 1});
-        }
-
-        // 더블
-        for (int i = 1; i <= 20; i++) {
-            scores.add(new int[]{i * 2, 0});
-        }
-
-        // 트리플
-        for (int i = 1; i <= 20; i++) {
-            scores.add(new int[]{i * 3, 0});
-        }
-
-        // 불
-        scores.add(new int[]{50, 1});
-
-        for (int i = 1; i <= target; i++) {
-            for (int[] scoreInfo : scores) {
-                int score = scoreInfo[0];
-                int singleBull = scoreInfo[1];
-
-                if (i < score) continue;
-
-                int throwCnt = dp[i - score][0] + 1;
-                int singleBullCnt = dp[i - score][1] + singleBull;
-
-                // 더 적은 다트 수
+        
+        List<Score> scores = getScores();
+        
+        for (int i = 1; i <= target; ++i) {
+            for (Score score : scores) {
+                if (i < score.point) continue;
+                
+                int throwCnt = dp[i - score.point][0] + 1;
+                int singleOrBullCnt = dp[i - score.point][1] + score.singleOrBull;
+                
                 if (throwCnt < dp[i][0]) {
                     dp[i][0] = throwCnt;
-                    dp[i][1] = singleBullCnt;
+                    dp[i][1] = singleOrBullCnt;
                 }
-                // 다트 수가 같으면 싱글/불 개수 최대
-                else if (throwCnt == dp[i][0] && singleBullCnt > dp[i][1]) {
-                    dp[i][1] = singleBullCnt;
+                else if (throwCnt == dp[i][0] && singleOrBullCnt > dp[i][1]) {
+                    dp[i][1] = singleOrBullCnt;
                 }
             }
         }
-
         return new int[]{dp[target][0], dp[target][1]};
+    }
+    
+    private List<Score> getScores() {
+        List<Score> scores = new ArrayList<>();
+        
+        for (int i = 1; i <= 20; ++i) {
+            // 싱글
+            scores.add(new Score(i, 1));
+            
+            // 더블
+            scores.add(new Score(i * 2, 0));
+            
+            // 트리플
+            scores.add(new Score(i * 3, 0));
+        }
+        // 불
+        scores.add(new Score(50, 1));
+        
+        return scores;
     }
 }

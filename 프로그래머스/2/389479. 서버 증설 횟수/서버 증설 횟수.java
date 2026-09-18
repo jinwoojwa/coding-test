c6dd00b482280ecb756cc7983c2ea7ec12207fdf
Jinwoo <112393728+jinwoojwa@users.noreply.github.com>
2026-09-18 14:31:07 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.66 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,47 +1,29 @@
 import java.util.*;
 
 class Solution {
-    
-    class Server {
-        int shutdownTime;
-        
-        Server(int shutdownTime) {
-            this.shutdownTime = shutdownTime;
-        }
-    }
-    
     public int solution(int[] players, int m, int k) {
         
-        Queue<Server> serverQ = new ArrayDeque<>();
-        int curServerCnt = 1;
+        Queue<Integer> serverQ = new ArrayDeque<>();
+        int runningServers = 1;
         int expansionCnt = 0;
         
         for (int time = 0; time <= 23; ++time) {
             int users = players[time];
             
-            System.out.println("TIME: " + time + " ~ " + (time + 1) + " USERS: " + users);
-            
             // 현재 시간대에 끝나는 서버 제거하기
-            while (!serverQ.isEmpty() && serverQ.peek().shutdownTime <= time) {
+            while (!serverQ.isEmpty() && serverQ.peek() <= time) {
                 serverQ.poll();
-                curServerCnt--;
+                runningServers--;
             }
             
-            System.out.println("[SYSTEM]SERVER: " + curServerCnt);
-            
-            if (users < m) {
-                System.out.println();
-                continue;
-            }
+            if (users < m) continue;
             
             // 현재 서버로 유저 감당 가능한 지 체크
-            while (curServerCnt * m <= users) {
-                curServerCnt++;
-                serverQ.offer(new Server(time + k));
+            while (runningServers * m <= users) {
+                runningServers++;
+                serverQ.offer(time + k);
                 expansionCnt++;
-                System.out.println("SERVER: " + curServerCnt + " EXPANSION: " + expansionCnt);
             }
-            System.out.println();
         }
         return expansionCnt;
     }
